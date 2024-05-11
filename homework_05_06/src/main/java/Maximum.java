226e943e82f83324a88916ee53900d31912f82b5
tolya <tolya@gmx.de>
2024-05-11 20:49:02 +0200

    добавил вывод наибольшего числа

--- homework_05_06/src/main/java/Maximum.java
+++ homework_05_06/src/main/java/Maximum.java
@@ -6,7 +6,7 @@ public class Maximum {
                             };
 
         if (isArray(numbers)) {
-            getMaximum(numbers);
+           System.out.println(getMaximum(numbers));
         }
         toPrint(numbers);
     }
