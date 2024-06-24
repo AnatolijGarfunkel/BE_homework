a94c85903525a0d719e917074b114faba8e2075f
tolya <tolya@gmx.de>
2024-06-24 16:14:51 +0200

    добавил то, что получилось сделать

--- homework_06_12/src/main/java/HW_06_12.java
+++ homework_06_12/src/main/java/HW_06_12.java
@@ -5,6 +5,44 @@ import java.util.function.UnaryOperator;
 
 public class HW_06_12 {
     /**
+     * Returns a {@link LongBinaryOperator} sum operation.
+     *
+     * @return binary sum operation
+     */
+    public static LongBinaryOperator longSumOperation() {
+        // throw new UnsupportedOperationException("It's your job to implement this method"); // todo
+        return (left, right) -> left + right;
+    }
+
+    /**
+     * Returns a {@link ToIntFunction<String>} that converts string to integer.
+     *
+     * @return string to int converter
+     */
+    public static ToIntFunction<String> stringToIntConverter() {
+        //throw new UnsupportedOperationException("It's your job to implement this method"); // todo
+        return new ToIntFunction<String>() {
+            @Override
+            public int applyAsInt(String value) {
+                return Integer.parseInt(value);
+            }
+        };
+    }
+
+    /**
+     * Receives int parameter n, and returns a {@link Supplier} that supplies {@link IntUnaryOperator}
+     * that is a function f(x) = n * x
+     *
+     * @param n a multiplier
+     * @return a function supplier
+     */
+    public static Supplier<IntUnaryOperator> nMultiplyFunctionSupplier(int n) {
+         //throw new UnsupportedOperationException("It's your job to implement this method"); // todo
+        return () -> operand -> operand * n;
+
+    }
+
+    /**
      * Returns a {@link UnaryOperator} that accepts str to str function and returns the same function composed with trim
      *
      * @return function that composes functions with trim() function
@@ -23,4 +61,5 @@ public class HW_06_12 {
             }
         };
     }
+//  на большее меня не хватило
 }
